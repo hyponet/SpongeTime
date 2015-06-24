09e2148e8df73913891fc490cfd277b614b36e5c
Coderhypo <i@ihypo.net>
2015-06-24 19:20:29 +0800

    create WorkFactory to create Work

---
+++ src/net/ihypo/work/WorkFactory.java
@@ -0,0 +1,28 @@
+package net.ihypo.work;
+
+public class WorkFactory {
+	static private int id = -1;
+	
+	static private int  getNextId(){
+		if(id == -1){
+			//调用数据库里的最新ID
+			id = 1;
+		}
+		
+		return id;
+	}
+	
+	static public Work createWork(String title,int userId,String data){
+		Work work =  new Work(getNextId(), title, userId, data);
+		id++;
+		
+		//写入数据库
+		insertWorkList(work);
+		
+		return work;
+	}
+	
+	static private void insertWorkList(Work work){
+		//写入数据库
+	}
+}
