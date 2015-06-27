3c9f55b86ee91babad72146441cd7a83a478a609
Coderhypo <i@ihypo.net>
2015-06-28 00:09:09 +0800

    手写数据库连接池

--- src/net/ihypo/db/DbDriver.java
+++ src/net/ihypo/db/DbDriver.java
@@ -2,21 +2,35 @@ package net.ihypo.db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class DbDriver {
-	private Connection connection;
-	private String url = "jdbc:mysql://localhost/SpongeTime";
-	private String user =  "root";
-	private String password = "password";
+	static private List<Connection> list;
+	static private String url = "jdbc:mysql://localhost/SpongeTime";
+	static private String user =  "root";
+	static private String password = "password";
+	static private int total = 20;
+	static private int now = 0;
 	
 	public DbDriver() throws ClassNotFoundException, SQLException{
-		Class.forName("com.mysql.jdbc.Driver");
-		connection = DriverManager.getConnection( url + 
-				"?user=" + user + "&password=" + password + "&useUnicode=true&characterEncoding=utf-8" );
+		if(list == null)
+		{
+			list = new ArrayList<Connection>();
+			Class.forName("com.mysql.jdbc.Driver");
+			for(int i = 0;i < total;i++){
+				list.add(DriverManager.getConnection( url + 
+						"?user=" + user + "&password=" + password + "&useUnicode=true&characterEncoding=utf-8" ));
+			}
+		}
 	}
 	
 	public Connection getConnection(){
-		return this.connection;
+		
+		int rtn = now;
+		now = (now + 1) % total;
+		return list.get(rtn);
 	}
+
 }
