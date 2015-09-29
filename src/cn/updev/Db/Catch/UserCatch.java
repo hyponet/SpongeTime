4d83809a46d250d5414128870e6102882fb96c7c
Coderhypo <i@ihypo.net>
2015-09-30 00:07:32 +0800

    unfinish user and event catch

---
+++ src/cn/updev/Db/Catch/UserCatch.java
@@ -0,0 +1,37 @@
+package cn.updev.Db.Catch;
+
+import cn.updev.Users.Static.IUser;
+
+import java.util.List;
+
+/**
+ * Created by hypo on 15-9-29.
+ */
+public class UserCatch {
+
+    public IUser getUserById(Integer userId){
+
+        return null;
+    }
+
+    public IUser getUserByLoginName(String loginName){
+
+        return null;
+    }
+
+    public IUser getUserByEMail(Integer eMail){
+
+        return null;
+    }
+
+
+    public List<IUser> getUsersByGroupId(Integer groupId){
+
+        return null;
+    }
+
+    public List<IUser> getAllUsers(){
+
+        return null;
+    }
+}
