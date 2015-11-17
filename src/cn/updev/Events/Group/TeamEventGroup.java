8ec2e9901103591652db6df7338cf26148e0016b
Coderhypo <i@ihypo.net>
2015-11-18 00:04:18 +0800

    add team evetn group

--- src/cn/updev/Events/Group/TeamEventGroup.java
+++ src/cn/updev/Events/Group/TeamEventGroup.java
@@ -1,7 +1,18 @@
 package cn.updev.Events.Group;
 
+import cn.updev.Events.Static.EventGroupWeight;
+import cn.updev.Events.Static.IEvent;
+import cn.updev.Events.Static.ITeamEvents;
+
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by hypo on 15-11-17.
  */
-public class TeamEventGroup {
+public class TeamEventGroup extends AbstractEventGroup implements ITeamEvents {
+
+    public TeamEventGroup(Date groupExpect, Integer groupId, String groupTitle, List<IEvent> list, Integer ownerId, EventGroupWeight weight) {
+        super(groupExpect, groupId, groupTitle, list, ownerId, weight);
+    }
 }
