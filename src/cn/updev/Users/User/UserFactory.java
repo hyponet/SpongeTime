38f23ec88ae7d929ddadb528c94703bc7572cd84
Coderhypo <i@ihypo.net>
2015-11-17 20:57:28 +0800

    update UserFactory

--- src/cn/updev/Users/User/UserFactory.java
+++ src/cn/updev/Users/User/UserFactory.java
@@ -1,6 +1,5 @@
 package cn.updev.Users.User;
 
-import cn.updev.Db.Update.UserUpdate;
 import cn.updev.Users.Static.IUser;
 import cn.updev.Users.Static.UserRule;
 
@@ -79,9 +78,6 @@ public class UserFactory {
 
         IUser user = new User(geteMail(),getNickName(),getPassWord(),UserRule.User,null,null,getUserName());
         //数据持久化并获得ID
-        UserUpdate update = new UserUpdate(user);
-
-        user = update.update();
 
         return user;
     }
