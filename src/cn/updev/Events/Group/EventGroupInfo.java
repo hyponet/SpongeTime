7e87e257037e6d847e8963643c9ba230064cb815
Coderhypo <i@ihypo.net>
2015-11-18 00:03:28 +0800

    add groupinfo

--- src/cn/updev/Events/Group/EventGroupInfo.java
+++ src/cn/updev/Events/Group/EventGroupInfo.java
@@ -1,7 +1,86 @@
 package cn.updev.Events.Group;
 
+import cn.updev.Events.Static.EventGroupWeight;
+
+import java.util.Date;
+
 /**
  * Created by hypo on 15-11-17.
  */
 public class EventGroupInfo {
+
+    private Integer groupId;
+    private String groupTitle;
+    private Date createTime;
+    private Date groupExpect;
+    private Date finishTime;
+    private EventGroupWeight weight;
+    private Integer ownerId;
+
+    public EventGroupInfo() {
+    }
+
+    public EventGroupInfo(Date groupExpect, String groupTitle, Integer ownerId, EventGroupWeight weight) {
+        this.createTime = new Date();
+        this.groupExpect = groupExpect;
+        this.groupTitle = groupTitle;
+        this.ownerId = ownerId;
+        this.weight = weight;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getFinishTime() {
+        return finishTime;
+    }
+
+    public void setFinishTime(Date finishTime) {
+        this.finishTime = finishTime;
+    }
+
+    public Date getGroupExpect() {
+        return groupExpect;
+    }
+
+    public void setGroupExpect(Date groupExpect) {
+        this.groupExpect = groupExpect;
+    }
+
+    public Integer getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Integer groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getGroupTitle() {
+        return groupTitle;
+    }
+
+    public void setGroupTitle(String groupTitle) {
+        this.groupTitle = groupTitle;
+    }
+
+    public Integer getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Integer ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public EventGroupWeight getWeight() {
+        return weight;
+    }
+
+    public void setWeight(EventGroupWeight weight) {
+        this.weight = weight;
+    }
 }
