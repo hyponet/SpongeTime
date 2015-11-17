8c97b6fee91952d7a2402f24ddec644994d5d690
Coderhypo <i@ihypo.net>
2015-11-18 00:03:59 +0800

    add user event group

--- src/cn/updev/Events/Group/UserEventGroup.java
+++ src/cn/updev/Events/Group/UserEventGroup.java
@@ -1,7 +1,18 @@
 package cn.updev.Events.Group;
 
+import cn.updev.Events.Static.EventGroupWeight;
+import cn.updev.Events.Static.IEvent;
+import cn.updev.Events.Static.IUserEvents;
+
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by hypo on 15-11-17.
  */
-public class UserEventGroup {
+public class UserEventGroup extends AbstractEventGroup implements IUserEvents {
+
+    public UserEventGroup(Date groupExpect, Integer groupId, String groupTitle, List<IEvent> list, Integer ownerId, EventGroupWeight weight) {
+        super(groupExpect, groupId, groupTitle, list, ownerId, weight);
+    }
 }
