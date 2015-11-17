d78296864be414201262e4802e5386e2f899d32b
Coderhypo <i@ihypo.net>
2015-11-17 17:25:06 +0800

    update HibernateSessionFactory

--- src/cn/updev/Database/HibernateSessionFactory.java
+++ src/cn/updev/Database/HibernateSessionFactory.java
@@ -1,7 +1,71 @@
 package cn.updev.Database;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Created by hypo on 15-11-17.
  */
 public class HibernateSessionFactory {
+
+    private static String CONFIG_FILE_LOCATION = "hibernate.cfg.xml";
+
+    /** Holds a single instance of Session */
+    private static final ThreadLocal threadLocal = new ThreadLocal();
+
+    /** The single instance of hibernate configuration */
+    private static final Configuration cfg = new Configuration();
+
+    /** The single instance of hibernate SessionFactory */
+    private static org.hibernate.SessionFactory sessionFactory;
+    /**
+     * Returns the ThreadLocal Session instance. Lazy initialize
+     * the SessionFactory if needed.
+     *
+     * @return Session
+     * @throws HibernateException
+     */
+
+    public static Session currentSession() throws HibernateException {
+        Session session = (Session) threadLocal.get();
+
+        if (session == null) {
+            if (sessionFactory == null) {
+                try {
+                    cfg.configure(CONFIG_FILE_LOCATION);
+                    sessionFactory = cfg.buildSessionFactory();
+                }
+                catch (Exception e) {
+                    System.err.println("%%%% Error Creating SessionFactory %%%%");
+                    e.printStackTrace();
+                }
+            }
+            session = sessionFactory.openSession();
+            threadLocal.set(session);
+        }
+
+        return session;
+    }
+
+    /**
+     * Close the single hibernate session instance.
+     *
+     * @throws HibernateException
+     */
+    public static void closeSession() throws HibernateException {
+        Session session = (Session) threadLocal.get();
+        threadLocal.set(null);
+
+        if (session != null) {
+            session.close();
+        }
+    }
+
+    /**
+     * Default constructor.
+     */
+    private HibernateSessionFactory() {
+    }
+
 }
