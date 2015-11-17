141c8de8614e5b82b68eb5cbd5f86ed33134cc38
Coderhypo <i@ihypo.net>
2015-11-18 02:26:55 +0800

    create EventGroupDAO

--- src/cn/updev/Events/Group/EventGroupDAO.java
+++ src/cn/updev/Events/Group/EventGroupDAO.java
@@ -1,7 +1,22 @@
 package cn.updev.Events.Group;
 
+import cn.updev.Database.HibernateSessionFactory;
+import org.hibernate.Session;
+
 /**
  * Created by hypo on 15-11-18.
  */
 public class EventGroupDAO {
+
+    private Session session;
+
+    public EventGroupDAO() {
+
+        this.session = HibernateSessionFactory.currentSession();
+    }
+
+    protected void finalize(){
+        HibernateSessionFactory.closeSession();
+    }
+
 }
