2cf476ebc700a261cd4c5f0798b5c78637ca0a58
Coderhypo <i@ihypo.net>
2015-11-19 21:55:58 +0800

    add EventAction

---
+++ src/cn/updev/Action/Event/EventAction.java
@@ -0,0 +1,9 @@
+package cn.updev.Action.Event;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * Created by hypo on 15-11-19.
+ */
+public class EventAction extends ActionSupport {
+}
---
+++ src/cn/updev/Action/Event/EventGroupAction.java
@@ -0,0 +1,7 @@
+package cn.updev.Action.Event;
+
+/**
+ * Created by hypo on 15-11-19.
+ */
+public class EventGroupAction {
+}
