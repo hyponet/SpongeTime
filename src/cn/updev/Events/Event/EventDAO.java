bdaa2c19caa8ae6bb52f83beba74d786feb06cbf
Coderhypo <i@ihypo.net>
2015-11-19 20:58:20 +0800

    update EventDAO

--- src/cn/updev/Events/Event/EventDAO.java
+++ src/cn/updev/Events/Event/EventDAO.java
@@ -2,8 +2,10 @@ package cn.updev.Events.Event;
 
 import cn.updev.Database.HibernateSessionFactory;
 import cn.updev.Events.Static.IEvent;
+import org.hibernate.Query;
 import org.hibernate.Session;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -18,24 +20,51 @@ public class EventDAO {
         this.session = HibernateSessionFactory.currentSession();
     }
 
-    public IEvent getEventById(Integer eventId){
+    public IEvent getEventById(Long eventId){
+        Query query =session.createQuery("from Event e where e.eventId=" + eventId);
 
-        return null;
+        if(query.list().size() == 0){
+            return null;
+        }
+        IEvent rnt = (IEvent) query.list().get(0);
+        return rnt;
     }
 
     public List<IEvent> getEventByUserId(Integer userId){
 
-        return null;
+        Query query =session.createQuery("from Event e where e.doerId=" + userId);
+
+        return (List<IEvent>) query.list();
     }
 
     public List<IEvent> getEventByEventGroupId(Integer eventGroupId){
 
-        return null;
+        Query query =session.createQuery("from Event e where e.groupId=" + eventGroupId);
+
+        return (List<IEvent>) query.list();
     }
 
     public List<IEvent> getEventByUserGroupId(Integer userGroupId){
 
         return null;
+    }
+
+    public Boolean eventFinish(Long eventId){
+
+        IEvent event = getEventById(eventId);
+        if(event.isFinish()){
+            event.setFinishTime(new Date());
+
+            session.update(event);
+
+            return true;
+        }else {
+            event.setFinishTime(null);
+
+            session.update(event);
+
+            return false;
+        }
     }
 
     protected void finalize(){
