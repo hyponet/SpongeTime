5a91fa5fa7c35f77517ef255750b59545ac00065
Coderhypo <i@ihypo.net>
2015-11-19 20:58:37 +0800

    update EventGroupDAO

--- src/cn/updev/Events/Group/EventGroupDAO.java
+++ src/cn/updev/Events/Group/EventGroupDAO.java
@@ -1,7 +1,14 @@
 package cn.updev.Events.Group;
 
 import cn.updev.Database.HibernateSessionFactory;
+import cn.updev.Events.Event.EventDAO;
+import cn.updev.Events.Static.IEvent;
+import cn.updev.Events.Static.ITeamEvents;
+import cn.updev.Events.Static.IUserEvents;
+import org.hibernate.Query;
 import org.hibernate.Session;
+
+import java.util.List;
 
 /**
  * Created by hypo on 15-11-18.
@@ -13,6 +20,30 @@ public class EventGroupDAO {
     public EventGroupDAO() {
 
         this.session = HibernateSessionFactory.currentSession();
+    }
+
+    public ITeamEvents getTeamEventGroup(Integer groupId){
+        Query query =session.createQuery("from EventGroupInfo info where info.groupId=" + groupId);
+
+        if(query.list().size() == 0){
+            return null;
+        }
+
+        List<IEvent> events = new EventDAO().getEventByEventGroupId(groupId);
+        EventGroupInfo group = (EventGroupInfo)query.list().get(0);
+        return new TeamEventGroup(group, events);
+    }
+
+    public IUserEvents getUserEventGroup(Integer groupId){
+        Query query =session.createQuery("from EventGroupInfo info where info.groupId=" + groupId);
+
+        if(query.list().size() == 0){
+            return null;
+        }
+
+        List<IEvent> events = new EventDAO().getEventByEventGroupId(groupId);
+        EventGroupInfo group = (EventGroupInfo)query.list().get(0);
+        return new UserEventGroup(group, events);
     }
 
     protected void finalize(){
