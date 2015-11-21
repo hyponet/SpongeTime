cf2a20373d7762471ae64bd81882981f43074f8c
Coderhypo <i@ihypo.net>
2015-11-22 00:45:17 +0800

    add DashboardAction

---
+++ src/cn/updev/Action/Admin/AdminDashboardAction.java
@@ -0,0 +1,10 @@
+package cn.updev.Action.Admin;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * 超级管理员仪表盘拦截器
+ * Created by hypo on 15-11-22.
+ */
+public class AdminDashboardAction extends ActionSupport {
+}
---
+++ src/cn/updev/Action/Admin/UserDashboardAction.java
@@ -0,0 +1,11 @@
+package cn.updev.Action.Admin;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * 用户仪表盘
+ * 作为用户后台的触发拦截器
+ * Created by hypo on 15-11-22.
+ */
+public class UserDashboardAction extends ActionSupport {
+}
