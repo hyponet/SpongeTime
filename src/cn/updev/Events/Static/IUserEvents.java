994d52e7b5f4b402f6b7deababf56d6d57fc02f8
Coderhypo <i@ihypo.net>
2015-11-23 02:46:08 +0800

    add func

--- src/cn/updev/Events/Static/IUserEvents.java
+++ src/cn/updev/Events/Static/IUserEvents.java
@@ -1,7 +1,13 @@
 package cn.updev.Events.Static;
 
+import cn.updev.Events.Group.EventGroupInfo;
+
+import java.util.List;
+
 /**
  * Created by hypo on 15-11-14.
  */
 public interface IUserEvents {
+    EventGroupInfo getGroupInfo();
+    List<IEvent> getList();
 }
