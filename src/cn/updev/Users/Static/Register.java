0f91839d933ab57360f30b19f2f8590e6e31038d
BLF2 <blf20822@126.com>
2015-11-22 21:12:16 +0800

    add a colum to GroupMemberInviteInfo and create class of Regiser

---
+++ src/cn/updev/Users/Static/Register.java
@@ -0,0 +1,20 @@
+package cn.updev.Users.Static;
+
+/**
+ * Created by blf2 on 15-11-22.
+ */
+
+import  cn.updev.Users.User.User;
+
+public class Register {
+    private User user;
+
+    public Register(User user){
+        this.user = user;
+    }
+
+    public boolean saveUserInfo(){
+        //存储user信息
+        return true;
+    }
+}
