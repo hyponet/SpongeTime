78915346caab618e6c19a40d51b523ebcf1b5cab
Coderhypo <i@ihypo.net>
2015-11-25 01:08:14 +0800

    update AdminAction

--- src/cn/updev/Action/Admin/AdminAction.java
+++ src/cn/updev/Action/Admin/AdminAction.java
@@ -1,6 +1,15 @@
 package cn.updev.Action.Admin;
 
+import cn.updev.EventWeight.Rate.EventGroupRateManage;
+import cn.updev.Events.Event.EventDAO;
+import cn.updev.Events.Group.EventGroupDAO;
+import cn.updev.Users.Static.Login;
 import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.ServletActionContext;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 /**
  * 作为后台的第一层拦截器
@@ -8,11 +17,78 @@ import com.opensymphony.xwork2.ActionSupport;
  * Created by hypo on 15-11-22.
  */
 public class AdminAction extends ActionSupport {
+
+    private String email;
+    private String password;
+
     public String login(){
-        return LOGIN;
+
+        if(this.email == null || this.password == null){
+            return LOGIN;
+        }
+
+        Login login = new Login(this.email, this.password);
+
+        if(login.judge()){
+
+            return SUCCESS;
+        }else {
+            return LOGIN;
+        }
     }
 
     public String execute() throws Exception {
+
+        // 获得用户ID并进行权限判断权限判断
+        Integer userId = 1;
+
+        //如果是普通用户
+        HttpServletRequest request = ServletActionContext.getRequest();
+        HttpSession session = request.getSession();
+        EventDAO eventDAO = new EventDAO();
+        EventGroupDAO groupDAO = new EventGroupDAO();
+
+        Integer allUserEventNum = eventDAO.getAllUserEventNum(userId);
+        Integer unFinishUserEventNum = eventDAO.getUnFinishUserEventNum(userId);
+        Integer allteamEventNum = eventDAO.getAllTeamEventNum(userId);
+        Integer unFinishteamEventNum = eventDAO.getUnFinishteamEventNum(userId);
+        Integer allFinishEventNum = allUserEventNum + allteamEventNum - unFinishUserEventNum - unFinishteamEventNum;
+
+        Integer allEventNum = allUserEventNum + allteamEventNum;
+        Integer eventFinishRate;
+
+        if(allEventNum == 0){
+            eventFinishRate = 0;
+        }else {
+            Integer finishEventNum = allEventNum - unFinishUserEventNum - unFinishteamEventNum;
+            eventFinishRate = 100 * finishEventNum / allEventNum;
+        }
+
+        session.setAttribute("unFinishUserEventNum", unFinishUserEventNum);
+        session.setAttribute("unFinishteamEventNum", unFinishteamEventNum);
+        session.setAttribute("allFinishEventNum", allFinishEventNum);
+        session.setAttribute("eventFinishRate", eventFinishRate);
+
+        List<Integer> groupIds = groupDAO.getUserEventGroupId(userId);
+        EventGroupRateManage rateManage = new EventGroupRateManage();
+        rateManage.setEventGroupRate(groupIds);
+
         return "user";
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
