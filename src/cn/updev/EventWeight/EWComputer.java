b2c5411a241e69b7934b452a4e6469371090052c
Coderhypo <i@ihypo.net>
2015-11-25 01:07:54 +0800

    add new file and del EWComputer.java

--- src/cn/updev/EventWeight/EWComputer.java
+++
@@ -1,8 +0,0 @@
-package cn.updev.EventWeight;
-
-/**
- * Created by hypo on 15-10-5.
- */
-public class EWComputer {
-
-}
---
+++ src/cn/updev/EventWeight/Rate/EventGroupRate.java
@@ -0,0 +1,7 @@
+package cn.updev.EventWeight.Rate;
+
+/**
+ * Created by hypo on 15-11-24.
+ */
+public class EventGroupRate {
+}
