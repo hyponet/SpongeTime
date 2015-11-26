0ef8eedb28875f4cad82d9c03fc0c347f54eb712
BLF2 <blf20822@126.com>
2015-11-26 19:34:20 +0800

    update fuction of Register

--- src/cn/updev/Users/Static/FuctionClass/Register.java
+++ src/cn/updev/Users/Static/FuctionClass/Register.java
@@ -4,17 +4,21 @@ package cn.updev.Users.Static.FuctionClass;
  * Created by blf2 on 15-11-22.
  */
 
-import  cn.updev.Users.User.User;
+import cn.updev.Users.Static.EnumeRule.UserRule;
+import cn.updev.Users.Static.UserOrGroupInterface.IUser;
+import cn.updev.Users.User.UserFactory;
 
 public class Register {
-    private User user;
+    private UserFactory userFactory = null;
 
-    public Register(User user){
-        this.user = user;
+    public Register(String userName, String nickName, String eMail, String passWord,String url){
+        userFactory = new UserFactory(userName,nickName,eMail,passWord,url,UserRule.User);
     }
 
-    public boolean saveUserInfo(){
-        //存储user信息
-        return true;
+    public IUser saveUserInfo(){
+        if(userFactory != null){
+            return userFactory.getUser();
+        }
+        return null;
     }
 }
