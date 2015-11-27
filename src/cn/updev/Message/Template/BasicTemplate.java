575029fde693f4da3fd2a66bb1066fdfabed3a68
Coderhypo <i@ihypo.net>
2015-11-27 18:24:25 +0800

    update BasicTemplate.java

--- src/cn/updev/Message/Template/BasicTemplate.java
+++ src/cn/updev/Message/Template/BasicTemplate.java
@@ -18,8 +18,8 @@ public class BasicTemplate {
 
     public String getContent(){
 
-        String rnt = "<body style=\"background-color: #f2f4f8\"><div style=\"margin-top: 10px; width: 90%; margin-left: 5%;box-shadow:2px 2px 5px #ccc;\">"+
-                "<div id=\"HEADER\" style=\"padding: 15px; background-color: #000; border-radius:10px 10px 0px 0px;\"><a class=\"navbar-brand\" href=\"http://todo.updev.cn\" style=\"text-decoration:none; color: #fff; font-size: 18px;\"><span style=\"color: #30a5ff; padding-left: 10px;\">SPONGE</span>TIME</a><span style=\"color: #ccc; font-size: 13px; padding: 10px;\"> 一个用心的时间助手</span></div>"+
+        String rnt = "<body style=\"background-color: #f2f4f8\"><div style=\"margin-top: 10px; width: 96%; margin-left: 2%;box-shadow:2px 2px 5px #ccc;\">"+
+                "<div id=\"HEADER\" style=\"padding: 15px; background-color: #000; border-radius:5px 5px 0px 0px;\"><a class=\"navbar-brand\" href=\"http://todo.updev.cn\" style=\"text-decoration:none; color: #fff; font-size: 18px;\"><span style=\"color: #30a5ff; padding-left: 10px;\">SPONGE</span>TIME</a><span style=\"color: #ccc; font-size: 13px; padding: 10px;\"> 一个用心的时间助手</span></div>"+
                 "<div id=\"BODY\" style=\"padding: 20px; background-color: #fff;\"><h2>"+this.title+"</h2>"+
                 "<p>" + this.content + "</p><hr/>" +
                 "<p style=\"font-size: 10px;\">* 本邮件由系统自动发送，请勿回复</p>" +
