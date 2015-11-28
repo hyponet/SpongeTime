30d17e196feaa8b974c3b851215ac7f793afd5eb
Coderhypo <i@ihypo.net>
2015-11-28 16:13:53 +0800

    update message template

--- src/cn/updev/Message/Template/BasicTemplate.java
+++ src/cn/updev/Message/Template/BasicTemplate.java
@@ -22,7 +22,7 @@ public class BasicTemplate {
                 "<div id=\"HEADER\" style=\"padding: 15px; background-color: #000; border-radius:5px 5px 0px 0px;\"><a class=\"navbar-brand\" href=\"http://todo.updev.cn\" style=\"text-decoration:none; color: #fff; font-size: 18px;\"><span style=\"color: #30a5ff; padding-left: 10px;\">SPONGE</span>TIME</a><span style=\"color: #ccc; font-size: 13px; padding: 10px;\"> 一个用心的时间助手</span></div>"+
                 "<div id=\"BODY\" style=\"padding: 20px; background-color: #fff;\"><h2>"+this.title+"</h2>"+
                 "<p>" + this.content + "</p><hr/>" +
-                "<p style=\"font-size: 10px;\">* 本邮件由系统自动发送，请勿回复</p>" +
+                "<p style=\"font-size: 10px;\">* 本邮件由系统自动发送，请勿回复 如需联系管理员，请致邮：i@ihypo.net</p>" +
                 "</div><div id=\"FOOTER\" style=\"padding: 10px; padding-left: 15px; background-color: #f6f6f6;\"><small style=\"color: #c0c0c0\">&copy;SpongeTime <a href=\"http://todo.updev.cn/login\" style=\"text-decoration:none\">Login</a></small>"+
                 "</div></div></body></html>";
 
