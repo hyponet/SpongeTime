496a3de66edd1481a1800f8fb8ca304934c38583
Coderhypo <i@ihypo.net>
2015-11-29 17:59:06 +0800

    update EventRemidTemplate

--- src/cn/updev/Message/Template/EventRemidTemplate.java
+++ src/cn/updev/Message/Template/EventRemidTemplate.java
@@ -24,6 +24,15 @@ public class EventRemidTemplate {
         this.userNickName = userNickName;
         this.event = event;
         this.eventWeight = eventWeight;
+
+        if(event.getGroupId() != null){
+
+            EventGroupInfo groupInfo = new EventGroupDAO().getEventGroupInfo(event.getGroupId());
+            this.eventTitle = groupInfo.getGroupTitle() + " " + event.getEventTitle();
+        }else {
+            this.eventTitle = event.getEventTitle();
+        }
+
     }
 
     public String getContent() {
@@ -37,10 +46,17 @@ public class EventRemidTemplate {
         if(sub > 0){
             status = "就目前进度来看，有可能提前完成。请继续加油！";
         }else {
-            status = "就目前进度来看，有可能无法按计划完成任务，还需努力。";
+            sub = -sub / (1000 * 3600 * 24);
+            if(sub > 4){
+
+                status = "就目前进度来看，进展过慢，很有可能无法按理想时间完成任务，还需按轻重缓急及时调整，继续努力。";
+            }else {
+
+                status = "就目前进度来看，进展稍慢，有可能无法按计划及时完成任务，还需努力。";
+            }
         }
 
-        this.content = "<p>客官 "+userNickName+"：</p><p>早上好！\n这是一封事件提醒邮件。</p>"+
+        this.content = "<p>客官 "+userNickName+"：</p><p>早上好！ 这是一封事件提醒邮件。</p>"+
                 "<p>您的事件 <b>" + eventTitle + "</b> ，曾计划于"+dateFormat.format(eventWeight.getEventExpect())+"完成，到您收到这封邮件为止，你的预计时间余额不足两天。</p>"+
                 "<p>我们预计这件事件将在"+dateFormat.format(eventWeight.getEventReckon())+"完成，"+status+"</p><p>小的汇报完毕，暂行告退。Have a Good Day！</p><p>还在加班的SpongeTime任务进度控制君敬上！</p><p>"+(new Date())+"</p>";
         return content;
