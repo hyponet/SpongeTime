a478b4195fd5ab886d2fd9cbe33867ec014f6cf8
Coderhypo <i@ihypo.net>
2015-12-04 04:23:46 +0800

    BLF2 update Notification

--- src/cn/updev/Users/NotificationPush/NotificationInfo.java
+++ src/cn/updev/Users/NotificationPush/NotificationInfo.java
@@ -1,6 +1,7 @@
 package cn.updev.Users.NotificationPush;
 
 import cn.updev.Users.Static.EnumeRule.NotificationStatus;
+import cn.updev.Users.Static.EnumeRule.NotificationType;
 import cn.updev.Users.Static.UserOrGroupDAO.UserOrGroupDelete;
 import cn.updev.Users.Static.UserOrGroupDAO.UserOrGroupSave;
 import cn.updev.Users.Static.UserOrGroupDAO.UserOrGroupUpdate;
@@ -13,15 +14,28 @@ public class NotificationInfo implements INotificationInfo{
     private Integer notificationInfoId;//没有实际意义，只是为了Hibernate
     private Integer notificationCreaterId;
     private Integer notificationAccepterId;
+    private NotificationType type;
     private String notifucationBody;
     private NotificationStatus status;
 
     public NotificationInfo(){}
-    public NotificationInfo(Integer notificationCreaterId,Integer notificationAccepterId,String notifucationBody,NotificationStatus status ){
+
+    public NotificationType getType() {
+        return type;
+    }
+
+    public void setType(NotificationType type) {
+        this.type = type;
+    }
+
+    public NotificationInfo(Integer notificationCreaterId,Integer notificationAccepterId,NotificationType type,
+                            String notifucationBody,NotificationStatus status ){
         this.notificationCreaterId = notificationCreaterId;
         this.notificationAccepterId = notificationAccepterId;
         this.notifucationBody = notifucationBody;
         this.status = status;
+        this.type = type;
+
     }
 
     public Integer getNotificationCreaterId() {
@@ -56,17 +70,6 @@ public class NotificationInfo implements INotificationInfo{
         this.status = status;
     }
 
-    public boolean isNotPush(){
-        if(this.status == NotificationStatus.notPush)
-            return true;
-        return false;
-    }
-
-    public boolean isHasPushed(){
-        if(this.status == NotificationStatus.notPush)
-            return true;
-        return false;
-    }
 
     public boolean isHasRead(){
         if(this.status == NotificationStatus.hasRead)
@@ -84,5 +87,13 @@ public class NotificationInfo implements INotificationInfo{
 
     public boolean deleteNotificationInfo(){
         return new UserOrGroupDelete().deleteNotificationInfo(this);
+    }
+
+    public Integer getNotificationInfoId() {
+        return notificationInfoId;
+    }
+
+    public void setNotificationInfoId(Integer notificationInfoId) {
+        this.notificationInfoId = notificationInfoId;
     }
 }
--- src/cn/updev/Users/NotificationPush/NotificationInfoFactory.java
+++ src/cn/updev/Users/NotificationPush/NotificationInfoFactory.java
@@ -1,6 +1,7 @@
 package cn.updev.Users.NotificationPush;
 
 import cn.updev.Users.Static.EnumeRule.NotificationStatus;
+import cn.updev.Users.Static.EnumeRule.NotificationType;
 import cn.updev.Users.Static.UserOrGroupInterface.INotificationInfo;
 
 /**
@@ -10,11 +11,19 @@ public class NotificationInfoFactory {
     private Integer notificationCreaterId;
     private Integer notificationAccepterId;
     private String notifucationBody;
+    private NotificationType type;
     private NotificationStatus status;
 
-    public NotificationInfoFactory(Integer notificationCreaterId,Integer notificationAccepterId,String notifucationBody,NotificationStatus status){
+    public NotificationType getType() {
+        return type;
+    }
+
+    public NotificationInfoFactory(Integer notificationCreaterId,Integer notificationAccepterId,NotificationType type,
+                                   String notifucationBody,NotificationStatus status){
         this.setNotificationCreaterId(notificationCreaterId);
         this.setNotificationAccepterId(notificationAccepterId);
+        this.setType(type);
+
         this.setNotifucationBody(notifucationBody);
     }
 
@@ -30,6 +39,9 @@ public class NotificationInfoFactory {
         if(body.length() > 100)
             body = body.substring(0,100);
         this.notifucationBody = body;
+    }
+    private void setType(NotificationType type){
+        this.type = type;
     }
 
     public Integer getNotificationCreaterId() {
@@ -49,11 +61,11 @@ public class NotificationInfoFactory {
     }
 
     public INotificationInfo getNotificationInfo(){
-        return new NotificationInfo(this.notificationCreaterId,this.notificationAccepterId,this.notifucationBody,NotificationStatus.notPush)
-                .saveNotificationInfo();
+        return new NotificationInfo(this.notificationCreaterId,this.notificationAccepterId,this.type,
+                this.notifucationBody,NotificationStatus.hasPushed).saveNotificationInfo();
     }
     public boolean updateNotificationInfo(){
-        return new NotificationInfo(this.notificationCreaterId,this.notificationAccepterId,this.notifucationBody,NotificationStatus.notPush)
+        return new NotificationInfo(this.notificationCreaterId,this.notificationAccepterId,this.type,this.notifucationBody,this.status)
                 .updateNotificationInfo();
     }
 }
